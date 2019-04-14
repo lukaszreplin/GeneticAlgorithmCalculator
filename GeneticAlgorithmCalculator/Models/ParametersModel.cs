aa976d63f08a74480af3d830e7287a4cbd1aae2d
Łukasz Replin <lukaszek016@gmail.com>
2019-04-14 21:54:25 +0200

    Initial commit

---
+++ GeneticAlgorithmCalculator/Models/ParametersModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithmCalculator.Models
+{
+    public class ParametersModel
+    {
+        public float RangeFrom { get; set; }
+
+        public float RangeTo { get; set; }
+
+        public int PopulationSize { get; set; }
+
+        public int NumberOfGenerations { get; set; }
+    }
+}
---
+++ GeneticAlgorithmCalculator/Models/Precision.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithmCalculator.Models
+{
+    public class Precision
+    {
+        public string Label { get; set; }
+
+        public float Value { get; set; }
+    }
+}
